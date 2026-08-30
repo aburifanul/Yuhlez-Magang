bdbc05d36667bf81e866681193eaa42f25daf017
aburifanul <aburifan62@gmail.com>
2026-08-30 17:29:49 +0700

    merubah file routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
 
 require __DIR__.'/root.php';
 require __DIR__.'/company.php';
